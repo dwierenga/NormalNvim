de14d5752536c13fb97a422ec1f49e392f1dcedd
Dan Wierenga <dwierenga@gmail.com>
2025-04-16 16:54:31 -0700

    cleanup some copy'n'paste detritus

--- lua/plugins/5-dpw-colorscheme.lua
+++ lua/plugins/5-dpw-colorscheme.lua
@@ -1,11 +1,7 @@
 
 return {
 
-  --  falcon [theme]
-  --  https://github.com/fenetikm/falcon
-  { "fenetikm/falcon" },
   { "arcticicestudio/nord-vim" },
-
   {
     "LazyVim/LazyVim",
     opts = {
