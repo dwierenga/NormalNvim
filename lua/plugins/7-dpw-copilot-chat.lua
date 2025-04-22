7913fa7c87be3d4bf473b284e1ee2b083a6fc4a0
Dan Wierenga <dwierenga@gmail.com>
2025-04-22 16:35:47 -0700

    add CopilotChat.nvim

---
+++ lua/plugins/7-dpw-copilot-chat.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    lazy = false,
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+    },
+  },
+}
